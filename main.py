ec4ce29ebe1a8936b3119356c028e6ea5611c6ef
omardavidcyber <rankupdavid@gmail.com>
2024-01-24 15:13:47 -0500

    create a report of the book text

---
+++ main.py
@@ -0,0 +1,40 @@
+def main():
+  with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+
+    words_in_file = count_words(file_contents)
+
+    letters_in_file = count_letters(file_contents)
+
+    create_book_report(words_in_file, letters_in_file) 
+
+
+def count_words(text):
+  text_array = text.split()
+  return len(text_array)
+
+def count_letters(text):
+  text = text.lower()
+  letters_count = {}
+  for char in text:
+    if char.isalpha():
+      if char in letters_count:
+        letters_count[char] += 1
+      else:
+        letters_count[char] = 1
+    
+  return letters_count
+
+def create_book_report(num_words, letters_dict):
+  print("--- begin report of books/frankenstein.txt ---")
+  print(f"{num_words} words found in the document")
+
+  letters_list = list(letters_dict)
+  letters_list.sort()
+
+  for letter in letters_list:
+    print(f"The {letter} character was found {letters_dict[letter]} times")
+
+  print("--- End report ---")
+
+main()
